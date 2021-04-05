f6006bdad07d1f8b338e4ef11a5cb45b3905107f
xx014939 <57679526+xx014939@users.noreply.github.com>
2021-04-05 14:05:12 +0100

    Login page

---
+++ finalYear/minimax/login.js
@@ -0,0 +1,33 @@
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      document.getElementById("loginDiv").style.display = "block"
+      document.getElementById("userDiv").style.display = "none"
+    } else {
+      // No user is signed in.
+      document.getElementById("loginDiv").style.display = "none"
+      document.getElementById("userDiv").style.display = "block"
+    }
+  });
+
+
+function login(){
+
+    var userEmail = document.getElementById("email-field").value;
+    var userPassword = document.getElementById("password-field").value;
+
+
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPassword)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("ERROR: "+ errorMessage);
+  });
+    
+}
