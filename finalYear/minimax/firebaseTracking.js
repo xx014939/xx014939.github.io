1009587c2b653dcbd024421fd195c6411c7289a0
xx014939 <57679526+xx014939@users.noreply.github.com>
2021-04-18 16:00:46 +0100

    firebase update

---
+++ finalYear/minimax/firebaseTracking.js
@@ -0,0 +1,22 @@
+
+var db = firebase.database();
+
+function userInteraction() {
+  db.ref('mostRecentUser/0/gameOne').set(' ')
+}
+
+//userInteraction();
+
+function userInteraction2() {
+  db.ref('mostRecentUser/0/gameTwo').set(' ')
+  
+}
+
+//userInteraction2()
+
+function userInteraction3() {
+  db.ref('mostRecentUser/0/gameThree').set(' ')
+  
+}
+
+//userInteraction3()
