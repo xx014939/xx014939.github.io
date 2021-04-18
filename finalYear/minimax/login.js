e54119d33e2ac2e6c35a3cc5350890d35b1bb444
xx014939 <57679526+xx014939@users.noreply.github.com>
2021-04-18 13:03:32 +0100

    Update login.js

--- finalYear/minimax/login.js
+++ finalYear/minimax/login.js
@@ -29,7 +29,7 @@ function login(){
     db.ref().set({
       testing: "working",
       mostRecentUser : {
-        gameOne: "?",
+        gameOne: " ",
         gameTwo: "?",
         gameThree: "?"
         }
