ea94ae247ec8462c2fc0d29ad4aa55287680fdb8
xx014939 <57679526+xx014939@users.noreply.github.com>
2021-04-18 12:45:06 +0100

    testing db with first interaction

--- finalYear/minimax/login.js
+++ finalYear/minimax/login.js
@@ -24,10 +24,10 @@ function login(){
     var user = userCredential.user;
     localStorage.setItem("currentUser",userEmail); // upon succesful login save current user to variable
 
-    var key = toString(userEmail);
+
     // create a child object unique to user, inside of firebase db and assign starting values to it
     db.ref().set({
-      [key] : {
+      currentUser : {
         game1: "?",
         game2: "?",
         game3: "?"}
