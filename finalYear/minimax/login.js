daf78b596939a81c6c9b3a08eee2a5e1f561ceec
xx014939 <57679526+xx014939@users.noreply.github.com>
2021-04-18 12:07:30 +0100

    Update login.js

--- finalYear/minimax/login.js
+++ finalYear/minimax/login.js
@@ -22,15 +22,15 @@ function login(){
   .then((userCredential) => {
     // Signed in
     var user = userCredential.user;
-    localStorage.setItem("currentUser",user); // upon succesful login save current user to variable
+    localStorage.setItem("currentUser",userEmail); // upon succesful login save current user to variable
 
     // create a child object unique to user, inside of firebase db and assign starting values to it
-    db.ref('game/' + user + '/game1').set('?')
-    db.ref('game/' + user + '/game1').on('value', snap => questionMark.innerText = snap.val());
-    db.ref('game/' + user + '/game2').set('?');
-    db.ref('game/' + user + '/game2').on('value', snap => questionMark.innerText = snap.val());
-    db.ref('game/' + user + '/game3').set('?');
-    db.ref('game/' + user + '/game3').on('value', snap => questionMark.innerText = snap.val());
+    db.ref('game/' + userEmail + '/game1').set('?')
+    db.ref('game/' + userEmail + '/game1').on('value', snap => questionMark.innerText = snap.val());
+    db.ref('game/' + userEmail + '/game2').set('?');
+    db.ref('game/' + userEmail + '/game2').on('value', snap => questionMark.innerText = snap.val());
+    db.ref('game/' + userEmail + '/game3').set('?');
+    db.ref('game/' + userEmail + '/game3').on('value', snap => questionMark.innerText = snap.val());
     window.location.href = "https://xx014939.github.io/finalYear/minimax/home";
     // ...
   })
